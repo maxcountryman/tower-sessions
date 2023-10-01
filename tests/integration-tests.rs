b03ee014d529602f66530f1ad746b40da3261337
Max Countryman <maxc@me.com>
2023-09-30 20:37:57 -0700

    rework mongodb store a bit

--- tests/integration-tests.rs
+++ tests/integration-tests.rs
@@ -129,7 +129,6 @@ mod mongodb_store_tests {
         let database_url = std::option_env!("MONGODB_URL").unwrap();
         let client = mongodb::Client::with_uri_str(database_url).await.unwrap();
         let session_store = MongoDBStore::new(client, "tower-sessions".to_string());
-        session_store.setup().await.unwrap();
         let session_manager = SessionManagerLayer::new(session_store).with_secure(true);
 
         build_app(session_manager, max_age)
