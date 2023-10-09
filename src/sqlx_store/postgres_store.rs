57630aa6b1f7d59123969d2c555d70969667b594
Max Countryman <maxc@me.com>
2023-10-09 14:02:58 -0700

    disable tokio feature for now

--- src/sqlx_store/postgres_store.rs
+++ src/sqlx_store/postgres_store.rs
@@ -91,7 +91,7 @@ impl PostgresStore {
         Ok(())
     }
 
-    #[cfg(feature = "tokio")]
+    //#[cfg(feature = "tokio")]
     /// This function will keep running indefinitely, deleting expired rows and
     /// then waiting for the specified period before deleting again.
     ///
