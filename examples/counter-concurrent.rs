298f1196abd4f782771b68de5a5974cf3fbf59bf
Max Countryman <maxc@me.com>
2023-10-15 14:10:56 -0700

    provide concurrent counter example

---
+++ examples/counter-concurrent.rs
@@ -0,0 +1,63 @@
+use std::net::SocketAddr;
+
+use axum::{
+    error_handling::HandleErrorLayer, response::IntoResponse, routing::get, BoxError, Router,
+};
+use http::StatusCode;
+use serde::{Deserialize, Serialize};
+use time::Duration;
+use tower::ServiceBuilder;
+use tower_sessions::{MemoryStore, Session, SessionManagerLayer};
+
+const COUNTER_KEY: &str = "counter";
+
+#[derive(Clone, Default, Deserialize, Serialize)]
+struct Counter(usize);
+
+#[tokio::main]
+async fn main() {
+    let session_store = MemoryStore::default();
+    let session_service = ServiceBuilder::new()
+        .layer(HandleErrorLayer::new(|_: BoxError| async {
+            StatusCode::BAD_REQUEST
+        }))
+        .layer(
+            SessionManagerLayer::new(session_store)
+                .with_secure(false)
+                .with_max_age(Duration::days(1)),
+        );
+
+    let app = Router::new()
+        .route("/", get(handler))
+        .layer(session_service);
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handler(session: Session) -> impl IntoResponse {
+    let mut counter: Counter = session
+        .get(COUNTER_KEY)
+        .expect("Could not deserialize.")
+        .unwrap_or_else(|| {
+            let counter = Counter::default();
+            session
+                .insert(COUNTER_KEY, counter.clone())
+                .expect("Could not serialize.");
+            counter
+        });
+
+    let mut new_counter = Counter(counter.0 + 1);
+    while let Ok(false) = session.replace_if_equal(COUNTER_KEY, counter.clone(), new_counter) {
+        counter = session
+            .get(COUNTER_KEY)
+            .expect("Could not deserialize.")
+            .unwrap();
+        new_counter = Counter(counter.0 + 1);
+    }
+
+    format!("Current count: {}", counter.0)
+}
