cb60f10c18660d16fc7198824bf423760a7c2c6f
Max Countryman <maxc@me.com>
2024-01-01 07:21:24 -0800

    make service infallible (#132)
    This reworks the tower service such that we always return a response. In
    other words, the service becomes infallible. What this means is we no
    longer need to build an intermediary service that captures boxed errors
    when using this crate with axum.
    
    One side effect of this is that we lose the ability to handle specific
    errors, such as the session save failing, via downcasting. Instead, the
    middleware will return a 500 response. To address this, we could extend
    this implementation to allow for an on error callback, which could take
    an error and return a response.
    
    This is a breaking change which also removes two extraneous variants
    from the session error type:
    
    - `MissingId`
    - `MissingCookies`

--- examples/mongodb-store.rs
+++ examples/mongodb-store.rs
@@ -1,12 +1,8 @@
 use std::net::SocketAddr;
 
-use axum::{
-    error_handling::HandleErrorLayer, response::IntoResponse, routing::get, BoxError, Router,
-};
-use http::StatusCode;
+use axum::{response::IntoResponse, routing::get, Router};
 use serde::{Deserialize, Serialize};
 use time::Duration;
-use tower::ServiceBuilder;
 use tower_sessions::{mongodb::Client, Expiry, MongoDBStore, Session, SessionManagerLayer};
 
 const COUNTER_KEY: &str = "counter";
@@ -19,19 +15,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let database_url = std::option_env!("DATABASE_URL").expect("Missing DATABASE_URL.");
     let client = Client::with_uri_str(database_url).await?;
     let session_store = MongoDBStore::new(client, "tower-sessions".to_string());
-    let session_service = ServiceBuilder::new()
-        .layer(HandleErrorLayer::new(|_: BoxError| async {
-            StatusCode::BAD_REQUEST
-        }))
-        .layer(
-            SessionManagerLayer::new(session_store)
-                .with_secure(false)
-                .with_expiry(Expiry::OnInactivity(Duration::seconds(10))),
-        );
+    let session_layer = SessionManagerLayer::new(session_store)
+        .with_secure(false)
+        .with_expiry(Expiry::OnInactivity(Duration::seconds(10)));
 
-    let app = Router::new()
-        .route("/", get(handler))
-        .layer(session_service);
+    let app = Router::new().route("/", get(handler)).layer(session_layer);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     let listener = tokio::net::TcpListener::bind(&addr).await?;
